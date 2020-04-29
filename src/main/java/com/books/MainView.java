1cf83c28965c44ea838872cad019369f16bbd618
TomaszTomasiak <55875232+TomaszTomasiak@users.noreply.github.com>
2020-04-29 21:35:13 +0200

    first

---
+++ src/main/java/com/books/MainView.java
@@ -0,0 +1,48 @@
+package com.books;
+
+import com.books.domain.Book;
+import com.books.domain.BookForm;
+import com.books.domain.BookService;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    private BookService bookService = BookService.getInstance();
+    private Grid<Book> grid = new Grid<>(Book.class);
+    private TextField filter = new TextField();
+    private BookForm form = new BookForm(this);
+
+
+    public MainView() {
+
+        filter.setPlaceholder("Filter by title");
+        filter.setClearButtonVisible(true);
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> update());
+        grid.setColumns("title", "author", "publicationYear", "type");
+        HorizontalLayout mainContent = new HorizontalLayout(grid, form);
+        mainContent.setSizeFull();
+        grid.setSizeFull();
+        form.setBook(null);
+
+        add(filter, mainContent);
+        setSizeFull();
+        refresh();
+        grid.asSingleSelect().addValueChangeListener(event -> form.setBook(grid.asSingleSelect().getValue()));
+
+    }
+
+    public void refresh() {
+        grid.setItems(bookService.getBooks());
+    }
+
+    public void update() {
+        grid.setItems(bookService.findByTitle(filter.getValue()));
+    }
+}
